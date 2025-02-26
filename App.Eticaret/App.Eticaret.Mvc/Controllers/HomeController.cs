b5d20fef7a06c29679b26f71ff350a6859c2dc13
Akif Akkoyun <144811507+Akif-Akkoyun@users.noreply.github.com>
2025-02-26 23:24:19 +0300

    added theme

--- App.Eticaret/App.Eticaret.Mvc/Controllers/HomeController.cs
+++ App.Eticaret/App.Eticaret.Mvc/Controllers/HomeController.cs
@@ -1,30 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
-namespace App.Eticaret.Mvc.Controllers;
 
-public class HomeController : Controller
+namespace App.Eticaret.Mvc.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
-    [HttpGet("/about-us")]
-    public IActionResult AboutUs()
-    {
-        return View();
-    }
-    [HttpGet("/contact")]
-    public IActionResult Contact()
-    {
-        return View();
-    }
-    [HttpGet("/product/{categoryName}/list")]
-    public IActionResult Listing()
-    {
-        return View();
-    }
-    [HttpGet("/product/{id:int}/details")]
-    public IActionResult ProductDetail()
-    {
-        return View();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("/about-us")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        [HttpGet("/contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet("/product/{categoryName}/list")]
+        public IActionResult Listing()
+        {
+            return View();
+        }
+        [HttpGet("/product/{id:int}/details")]
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
     }
 }
--- App.Eticaret/App.Eticaret.Mvc/Program.cs
+++ App.Eticaret/App.Eticaret.Mvc/Program.cs
@@ -4,6 +4,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -11,9 +12,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
